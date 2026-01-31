8246d47a5896daa6e9e1d89e0e636134f01a7954
samsudinMohamad <sam88mohd@gmail.com>
2026-01-31 21:45:44 +0800

    add niri kdl

--- home.nix
+++ home.nix
@@ -17,6 +17,7 @@
   ];
 
   home.file = {
+    ".config/niri/config.kdl".source = ./niri/default-config.kdl;
   };
 
   home.sessionVariables = {
@@ -49,74 +50,5 @@
   programs.niri = {
     enable = true;
     package = pkgs.niri-unstable;
-    settings = {
-      binds = {
-        # General
-        "Mod+Shift+Slash".action."show-hotkey-overlay" = [];
-        "Mod+Return".action.spawn = "kitty";
-        "Mod+Shift+Return".action.spawn = "zen";
-        "Mod+q".action.close-window = [];
-         
-        # Navigation
-        "Mod+Left".action.focus-column-left = [];
-        "Mod+Right".action.focus-column-right = [];
-        "Mod+Down".action.focus-window-down = [];
-        "Mod+Up".action.focus-window-up = [];
-        
-        "Mod+Ctrl+Left".action.move-column-left = [];
-        "Mod+Ctrl+Right".action.move-column-right = [];
-        "Mod+Ctrl+Down".action.move-window-down = [];
-        "Mod+Ctrl+Up".action.move-window-up = [];
-
-        "Mod+Home".action.focus-column-first = [];
-        "Mod+End".action.focus-column-last = [];
-        "Mod+Ctrl+Home".action.move-column-to-first = [];
-        "Mod+Ctrl+End".action.move-column-to-last = [];
-        
-        "Mod+Shift+Left".action.focus-monitor-left = [];
-        "Mod+Shift+Down".action.focus-monitor-down = [];
-        "Mod+Shift+Up".action.focus-monitor-up = [];
-        "Mod+Shift+Right".action.focus-monitor-right = [];
-        
-        "Mod+Shift+Ctrl+Left".action.move-column-to-monitor-left = [];
-        "Mod+Shift+Ctrl+Down".action.move-column-to-monitor-down = [];
-        "Mod+Shift+Ctrl+Up".action.move-column-to-monitor-up = [];
-        "Mod+Shift+Ctrl+Right".action.move-column-to-monitor-right = [];
-
-        "Mod+Page_Down".action.focus-workspace-down = [];
-        "Mod+Page_Up".action.focus-workspace-up = [];
-        "Mod+U".action.focus-workspace-down = [];
-        "Mod+I".action.focus-workspace-up = [];
-        "Mod+Ctrl+Page_Down".action.move-column-to-workspace-down = [];
-        "Mod+Ctrl+Page_Up".action.move-column-to-workspace-up = [];
-        "Mod+Ctrl+U".action.move-column-to-workspace-down = [];
-        "Mod+Ctrl+I".action.move-column-to-workspace-up = [];
-        "Mod+Shift+Page_Down".action.move-workspace-down = [];
-        "Mod+Shift+Page_Up".action.move-workspace-up = [];
-        "Mod+Shift+U".action.move-workspace-down = [];
-        "Mod+Shift+I".action.move-workspace-up = [];
-        
-        "Mod+Comma".action.consume-window-into-column = [];
-        "Mod+Period".action.expel-window-from-column = [];
-        
-        "Mod+R".action.switch-preset-column-width = [];
-        "Mod+F".action.maximize-column = [];
-        "Mod+Shift+F".action.fullscreen-window = [];
-        "Mod+C".action.center-column = [];
-        
-        # Screenshot 
-        "Print".action.screenshot = [];
-        "Ctrl+Print".action.screenshot-screen = [];
-        "Alt+Print".action.screenshot-window = [];
-
-        # Quit
-        "Mod+Shift+E".action.quit = [];
-        "Mod+Shift+P".action.power-off-monitors = [];
-
-        # Volume and brightness control
-        "XF86AudioRaiseVolume".action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+"];
-        "XF86AudioLowerVolume".action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-"];
-      };
-    };
   };
 }
