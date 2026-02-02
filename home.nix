d703243e10a0fd0b07805b61f0eb7ebcef2c9be1
samsudinMohamad <sam88mohd@gmail.com>
2026-02-02 11:34:05 +0800

    make niri config mkOutofSymlink

--- home.nix
+++ home.nix
@@ -1,73 +1,63 @@
 { inputs, config, pkgs, lib, ... }:
 
-let
-  noctalia = cmd: [
-    "noctalia-shell" "ipc" "call"
-  ] ++ (pkgs.lib.splitString " " cmd);
-in
- {
-   imports = [ 
-     inputs.niri.homeModules.niri
-     inputs.noctalia.homeModules.default
-   ];
- 
-   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
-   
-   home.username = "sm";
-   home.homeDirectory = lib.mkDefault "/home/sm";
- 
-   home.stateVersion = "25.11"; # Please read the comment before changing.
- 
-   home.packages = with pkgs; [
-     inputs.zen-browser.packages.${system}.default
-     alacritty 
-     python3
-     nautilus
-   ];
- 
-   home.file = {
-     ".config/niri/config.kdl".source = ./niri/default-config.kdl;
-   };
- 
-   home.sessionVariables = {
-     EDITOR = "vim";
-   };
- 
-   programs.git = {
-     enable = true;
-     extraConfig = {
-       user.name = "samsudinMohamad";
-       user.email = "sam88mohd@gmail.com";
-       init.defaultBranch = "main";      
-     };
-   };
- 
-   programs.vim = {
-     enable = true;
-     settings = {
-       tabstop = 2;
-     };
-   };
- 
-   # Let Home Manager install and manage itself.
-   programs.home-manager.enable = true;
- 
-   # niri config
-   programs.niri = {
-     enable = true;
-     package = pkgs.niri-unstable;
-     settings = {
-       binds = with config.lib.niri.actions; {
-        "Mod+space".action.spawn = noctalia "launcher toggle";
-       };
-     };
-   };
- 
-   # enable noctaslia systemd service
-   programs.noctalia-shell.systemd.enable = true;
- 
-   # configure noctalia shell
-   programs.noctalia-shell = {
-     enable = true;
-   };
+{
+  imports = [ 
+    inputs.niri.homeModules.niri
+    inputs.noctalia.homeModules.default
+  ];
+
+  nixpkgs.overlays = [ inputs.niri.overlays.niri ];
+  
+  home.username = "sm";
+  home.homeDirectory = lib.mkDefault "/home/sm";
+
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    inputs.zen-browser.packages.${system}.default
+    alacritty 
+    python3
+    nautilus
+  ];
+
+  home.file = {
+    ".config/niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink /home/sm/.config/home-manager/niri/default-config.kdl;
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      user.name = "samsudinMohamad";
+      user.email = "sam88mohd@gmail.com";
+      init.defaultBranch = "main";      
+    };
+  };
+
+  programs.vim = {
+    enable = true;
+    settings = {
+      tabstop = 2;
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # niri config
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri-unstable;
+  };
+
+  # enable noctalia systemd service
+  programs.noctalia-shell.systemd.enable = true;
+
+  # configure noctalia shell
+  programs.noctalia-shell = {
+    enable = true;
+  };
  }
